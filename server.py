766e1e1f3c97953c8403e2a17bfc84d3bea182f4
verificaicode <oiiaiiocat@gmail.com>
2025-07-18 21:02:20 -0300

    update

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ from controls_input import ControlsInput
 from verify_links import VerifyLinks
 import asyncio
 from threading import Thread
+import time
 
 class Server(ControlsInput, VerifyLinks):
     def __init__(self):
@@ -30,17 +31,19 @@ class Server(ControlsInput, VerifyLinks):
         self.register_routes()
 
         if self.DEBUG:
-            self.connect_to_server(3)
+            self.connect_to_server()
     
-    def connect_to_server(self, attempts):
-        try:
-            self.io.connect(self.VERIFICA_AI_PROXY)
-        
-        # Erro ao tentar se conectar com o servidor
-        except socketio.exceptions.ConnectionError:
-            attempts -= 1
-            if attempts > 0:
-                self.connect_to_server(attempts)
+    def connect_to_server(self):
+        while True:
+            try:
+                self.io.connect(self.VERIFICA_AI_PROXY)
+                time.sleep(1)
+            # Erro ao tentar se conectar com o servidor
+            except socketio.exceptions.ConnectionError:
+                pass
+
+            else:
+                break
 
 
 
@@ -49,6 +52,7 @@ class Server(ControlsInput, VerifyLinks):
 
     def disconnect(self):
         print("Desconectado do servidor.")
+        self.connect_to_server
 
     def register_routes(self):
         self.io.on("connect", self.connect)
